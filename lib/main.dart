3da12aec77eb6b9038563277c623e14cadca70a4
akiev33 <71421550+akiev33@users.noreply.github.com>
2022-08-24 02:34:15 +0600

    test commit

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        backgroundColor: Colors.red[900],
+        appBar: AppBar(
+          centerTitle: false,
+          title: const Text('Sample App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              child: Text(
+                'image',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Container(
+              height: 105,
+              width: double.infinity,
+              decoration: const BoxDecoration(color: Colors.green),
+              child: Expanded(
+                child: ListView(
+                  children: [
+                    TextAdd(),
+                    TextAdd(),
+                    TextAdd(),
+                    TextAdd(),
+                    TextAdd(),
+                    TextAdd(),
+                    TextAdd(),
+                    TextAdd(),
+                    TextAdd(),
+                    TextAdd(),
+                    TextAdd(),
+                    TextAdd(),
+                    TextAdd(),
+                  ],
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextAdd extends StatelessWidget {
+  const TextAdd({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.all(17),
+          height: 70,
+          width: 85,
+          color: Colors.orange,
+          child: Container(
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                '  +\nAdd',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
